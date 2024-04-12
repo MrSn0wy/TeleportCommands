b696c914170779fd5fd5b4bd6a6a56025dd7db44
Mr. Snowy <61592704+MrSn0wy@users.noreply.github.com>
2024-04-13 01:19:15 +0200

    basics

---
+++ src/client/java/dev/mrsnowy/teleport_commands/TeleportCommandsClient.java
@@ -0,0 +1,10 @@
+package dev.mrsnowy.teleport_commands;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeleportCommandsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
